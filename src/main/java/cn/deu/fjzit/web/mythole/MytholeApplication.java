d7bb26d1e2749a698092245398a81636b59c60ba
Abei-art <1976235348@qq.com>
2019-10-06 14:01:24 +0800

    Initial commit

---
+++ src/main/java/cn/deu/fjzit/web/mythole/MytholeApplication.java
@@ -0,0 +1,13 @@
+package cn.deu.fjzit.web.mythole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MytholeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MytholeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/deu/fjzit/web/mythole/MytholeApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.deu.fjzit.web.mythole;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MytholeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
